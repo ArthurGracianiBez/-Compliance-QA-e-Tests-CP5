c338cd94156bfc88e42259a1bc1b026dad130fa6
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-09-11 19:42:01 -0300

    feat: add assertions dependencie

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @DisplayName("Checkpoint 5 - Login")
 public class Login {
 
