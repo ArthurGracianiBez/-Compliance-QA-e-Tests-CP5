6af95799c230e86db634260e7833fc94f2959700
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-09-11 20:10:56 -0300

    feat: add fildelement ct1

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -13,8 +13,8 @@ public class Login {
     private static final String USUARIO_VALIDO = "standard_user";
     private static final String SENHA_VALIDA = "secret_sauce";
 
-    private static final By CAMPO_USUARIO = By.tagName("");
-    private static final By CAMPO_SENHA = By.tagName("");
+    private static final By CAMPO_USUARIO = By.id("user-name");
+    private static final By CAMPO_SENHA = By.tagName("password");
     private static final By BOTAO_LOGIN = By.tagName("");
     private static final By ICONE_CARRINHO = By.tagName("");
 
@@ -31,9 +31,13 @@ public class Login {
     @Test
     @DisplayName("CT1 - Login com sucesso")
     void deveLogarComCredenciaisValidas(){
+        // Dado: que esteja na pagina saucedemo.com
         driver.get(BASE_URL);
         assertEquals(BASE_URL, driver.getCurrentUrl());
         assertEquals("Swag Labs", driver.getTitle());
+
+        driver.findElement(CAMPO_USUARIO).sendKeys(USUARIO_VALIDO);
+        driver.findElement(CAMPO_SENHA).sendKeys(SENHA_VALIDA);
     }
 
 }
