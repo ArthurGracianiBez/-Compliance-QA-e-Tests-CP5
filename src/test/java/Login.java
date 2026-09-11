639328fda18a8723d872e9cca9ad21f0ccff04de
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-09-11 19:39:02 -0300

    feat: create login class

---
+++ src/test/java/Login.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+@DisplayName("Checkpoint 5 - Login")
+public class Login {
+
+
+}
