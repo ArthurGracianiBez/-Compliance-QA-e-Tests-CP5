59bbe35125553b263467afb645c613699020800b
ArthurGracianiBez <arthur.graciani@gmail.com>
2026-09-17 22:38:48 -0300

    feat: add web driver wait

--- src/test/java/Login.java
+++ src/test/java/Login.java
@@ -2,6 +2,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -9,18 +17,23 @@ import static org.junit.jupiter.api.Assertions.*;
 public class Login {
 
     private WebDriver driver;
+    private WebDriverWait wait;
     private static final String BASE_URL = "https://www.saucedemo.com/";
     private static final String USUARIO_VALIDO = "standard_user";
     private static final String SENHA_VALIDA = "secret_sauce";
+    private static final String USUARIO_BLOQUEADO = "locked_out_user";
+    private static final String SENHA_INVALIDA = "senha_incorreta_123";
 
     private static final By CAMPO_USUARIO = By.id("user-name");
     private static final By CAMPO_SENHA = By.id("password");
     private static final By BOTAO_LOGIN = By.id("login-button");
     private static final By ICONE_CARRINHO = By.id("shopping_cart_container");
+    private static final By MENSAGEM_ERRO = By.cssSelector("[data-test='error']");
 
     @BeforeEach
     void abrirNavegador(){
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
     @AfterEach
@@ -29,7 +42,7 @@ public class Login {
     }
 
     @Test
-    @DisplayName("CT1 - Login com sucesso")
+    @DisplayName("CT1 - 200 (Login com sucesso)")
     void deveLogarComCredenciaisValidas(){
         // Dado: que esteja na pagina saucedemo.com
         driver.get(BASE_URL);
@@ -41,6 +54,7 @@ public class Login {
         driver.findElement(CAMPO_SENHA).sendKeys(SENHA_VALIDA);
 
         // E: clicar no botao "Login"
+
         driver.findElement(BOTAO_LOGIN).click();
 
         // Entao: devera ser redirecionado para a pagina inventory.html
@@ -49,5 +63,6 @@ public class Login {
         // validar se redirecionou para a pagina inventory.html
         assertTrue(driver.findElement(ICONE_CARRINHO).isDisplayed());
     }
+
 
 }
